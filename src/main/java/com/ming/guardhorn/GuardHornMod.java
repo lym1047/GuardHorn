6644ca488998f4621be286a4bb75e22c65c00cb5
Ming <140613433+lym1047@users.noreply.github.com>
2026-08-31 09:32:48 +0800

    Update GuardHornMod.java——同步1.20.1

--- src/main/java/com/ming/guardhorn/GuardHornMod.java
+++ src/main/java/com/ming/guardhorn/GuardHornMod.java
@@ -2,19 +2,20 @@ package com.ming.guardhorn;
 
 import com.ming.guardhorn.config.ModCommonConfig;
 import com.ming.guardhorn.event.GuardHornEvent;
+import com.ming.guardhorn.event.IllagerExtraTargetHandler;
 import com.ming.guardhorn.event.ZombieHitIllagers;
 import com.ming.guardhorn.item.ModItems;
-import org.slf4j.Logger;
-
 import com.mojang.logging.LogUtils;
-
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import org.slf4j.Logger;
 
 @Mod(GuardHornMod.MOD_ID)
 public class GuardHornMod {
@@ -23,15 +24,19 @@ public class GuardHornMod {
 
     public GuardHornMod(IEventBus modEventBus, ModContainer modContainer) {
         modContainer.registerConfig(ModConfig.Type.COMMON, ModCommonConfig.SPEC, "guardhorn-common.toml");
+
         ModItems.register(modEventBus);
-        NeoForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
+
         NeoForge.EVENT_BUS.register(new GuardHornEvent());
         NeoForge.EVENT_BUS.register(new ZombieHitIllagers());
-
-        modEventBus.register(ModCommonConfig.class);
+        NeoForge.EVENT_BUS.register(new IllagerExtraTargetHandler());
     }
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-        LOGGER.info("Ming");
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+            event.accept(ModItems.GUARD_HORN);
+        }
     }
 }
