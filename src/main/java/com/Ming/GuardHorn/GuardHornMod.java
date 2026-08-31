eaed7a202790322685d876f9f7b8d7fa3ba7aac2
Ming <140613433+lym1047@users.noreply.github.com>
2026-08-31 09:21:08 +0800

    Update GuardHornMod.java——移植到1.21.1

--- src/main/java/com/Ming/GuardHorn/GuardHornMod.java
+++ src/main/java/com/Ming/GuardHorn/GuardHornMod.java
@@ -1,32 +1,42 @@
-package com.Ming.GuardHorn;
+package com.ming.guardhorn;
 
-import com.Ming.GuardHorn.event.VillagerHornEvent;
-import com.Ming.GuardHorn.event.ZombieHitIllagers;
-import com.Ming.GuardHorn.item.ModItems;
-import com.Ming.GuardHorn.config.ModCommonConfig;
+import com.ming.guardhorn.config.ModCommonConfig;
+import com.ming.guardhorn.event.GuardHornEvent;
+import com.ming.guardhorn.event.IllagerExtraTargetHandler;
+import com.ming.guardhorn.event.ZombieHitIllagers;
+import com.ming.guardhorn.item.ModItems;
 import com.mojang.logging.LogUtils;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import org.slf4j.Logger;
 
 @Mod(GuardHornMod.MOD_ID)
 public class GuardHornMod {
     public static final String MOD_ID = "guardhorn";
-    private static final Logger LOGGER = LogUtils.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
 
-    public GuardHornMod() {
+    public GuardHornMod(IEventBus modEventBus, ModContainer modContainer) {
+        modContainer.registerConfig(ModConfig.Type.COMMON, ModCommonConfig.SPEC, "guardhorn-common.toml");
 
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ModCommonConfig.SPEC, "guardhorn-common.toml");
-
-        MinecraftForge.EVENT_BUS.register(new VillagerHornEvent());
-
-        MinecraftForge.EVENT_BUS.register(new ZombieHitIllagers());
-
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         ModItems.register(modEventBus);
+
+        modEventBus.addListener(this::addCreative);
+
+        NeoForge.EVENT_BUS.register(new GuardHornEvent());
+        NeoForge.EVENT_BUS.register(new ZombieHitIllagers());
+        NeoForge.EVENT_BUS.register(new IllagerExtraTargetHandler());
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+            event.accept(ModItems.GUARD_HORN);
+        }
     }
 }
