a763644ebf7454e86c35b74507cccc19872331ac
Ming <140613433+lym1047@users.noreply.github.com>
2026-08-31 09:22:03 +0800

    Update GuardHornItem.java——同步1.20.1

--- src/main/java/com/Ming/GuardHorn/features/GuardHornItem.java
+++ src/main/java/com/Ming/GuardHorn/features/GuardHornItem.java
@@ -1,17 +1,20 @@
-package com.Ming.GuardHorn.features;
+package com.ming.guardhorn.features;
 
-import com.Ming.GuardHorn.config.ModCommonConfig;
+import com.ming.guardhorn.config.ModCommonConfig;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
 
 public class GuardHornItem extends Item {
 
@@ -21,37 +24,32 @@ public class GuardHornItem extends Item {
 
     @Override
     public @NotNull UseAnim getUseAnimation(@NotNull ItemStack stack) {
-        return UseAnim.TOOT_HORN; // 号角动画
+        return UseAnim.TOOT_HORN;
     }
 
     @Override
-    public int getUseDuration(@NotNull ItemStack stack) {
-        return 40; // 原版山羊角为 40 ticks
+    public int getUseDuration(@NotNull ItemStack stack, @Nullable LivingEntity entity) {
+        return 40;
     }
 
+
     @Override
-    public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level level, Player player, @NotNull InteractionHand hand) {
+    public @NotNull InteractionResultHolder<ItemStack> use(Level level, Player player, @NotNull InteractionHand hand) {
         ItemStack stack = player.getItemInHand(hand);
 
         var hornVariants = SoundEvents.GOAT_HORN_SOUND_VARIANTS;
         SoundEvent hornSound = hornVariants.get(level.random.nextInt(hornVariants.size())).value();
-
         if (hornSound == null) {
             // 播放一个实体的声音作为错误提示
             hornSound = SoundEvents.GOAT_HORN_BREAK;
         }
-
-
         //服务端向附近玩家广播，客户端听到
         level.playSound(null, player.getX(), player.getY(), player.getZ(),
-                hornSound, SoundSource.PLAYERS,
-                1.0F, 1.0F); // 原版山羊角的音效
+                hornSound, SoundSource.PLAYERS, 1.0F, 1.0F);
 
-        player.getCooldowns().addCooldown(this, ModCommonConfig.cooldownTicks);
-
+        player.getCooldowns().addCooldown(this, ModCommonConfig.CONFIG.getCooldownTicks());
         player.startUsingItem(hand);
         //返回 success 表示使用成功但不消耗物品
-        return InteractionResultHolder.success(stack);
+        return InteractionResultHolder.consume(stack);
     }
-
 }
